a53eebd85fcebfaf2199bc2b86526a8b6792087f
Matt Fraser <mefcon@hotmail.com>
2016-07-12 09:44:44 -0400

    Add project files.

---
+++ FGAG/FTClient.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenPop.Pop3;
+using OpenPop.Mime;
+using System.IO;
+using System.Net.Mail;
+
+namespace FGAG
+{
+    public class FTClient
+    {
+
+        public  void GetAttachments(string hostname, int port, bool useSsl, string username, string password, string savelocation)
+        {
+            // The client disconnects from the server when being disposed
+            using (Pop3Client client = new Pop3Client())
+            {
+                // Connect to the server
+                client.Connect(hostname, port, useSsl);
+               
+                // Authenticate ourselves towards the server
+                client.Authenticate(username, password);
+
+                // Get the number of messages in the inbox
+                int messageCount = client.GetMessageCount();
+
+                // We want to download all messages
+                List<Message> allMessages = new List<Message>(messageCount);
+
+                // Messages are numbered in the interval: [1, messageCount]
+                // Ergo: message numbers are 1-based.
+                // Most servers give the latest message the highest number
+                for (int i = messageCount; i > 0; i--)
+                {
+                    var mess = client.GetMessage(i);
+                
+                    if (mess.Headers.Subject.ToLower().Contains("current"))
+                    {
+                        var atts = mess.FindAllAttachments();
+                        foreach (MessagePart attachment in atts)
+                        {
+                            string filePath = Path.Combine(@"C:\Attachment", attachment.FileName);
+                         
+                                FileStream Stream = new FileStream(filePath, FileMode.Create);
+                                BinaryWriter BinaryStream = new BinaryWriter(Stream);
+                                BinaryStream.Write(attachment.Body);
+                                BinaryStream.Close();
+                            
+                        }
+                        client.DeleteMessage(i);
+                    }
+
+
+           
+                }
+
+                // Now return the fetched messages
+            
+            }
+        }
+
+        
+        }
+    }
+
---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            FGAG.FTClient c = new FGAG.FTClient();
+            c.GetAttachments("pop.gmail.com", 995, true, "hdcfieldtrace@gmail.com", "Inger$0ll", "c");
+        }
+    }
+}
